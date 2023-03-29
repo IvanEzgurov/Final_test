96cb0247a073f6cc851caa1517f2874d0ec3e4c9
Ivan_Ezgurov <ezgurovivan@gmail.com>
2023-03-30 01:40:47 +0300

    test

--- test/Program.cs
+++ test/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк 
+//формирует массив из строк, длина которых меньше либо равна 3 символа. 
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+//При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
+
+string [] startArray = new string [6] {"1236", "start", "12", "exe", "-45", "date"};
+string [] finalArray = new string [startArray.Length];
+int count = 0;
+void SortArray(string[] startArray, string [] finalArray)
+{
+    for (int i = 0; i < startArray.Length; i++)
+    {
+        if (startArray[i].Length <= 3)
+        {
+            finalArray[count] = startArray[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length -1)
+        {
+            Console.Write($"{array[i]}");
+            Console.Write("]");
+        }
+        else
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+SortArray(startArray, finalArray);
+Array.Resize(ref finalArray, count);
+PrintArray(finalArray);
